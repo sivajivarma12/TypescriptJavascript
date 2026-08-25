2e08fef6b18d4122ff6652ee9904e46c996627b7
sivajivarma12 <sivajivarma12@gmail.com>
2026-08-25 07:46:51 +0530

    hello world

---
+++ javascript-training/javascript-basics/hello-world.js
@@ -0,0 +1,52 @@
+console.log("Hello, World !!");
+
+//console method are log warn info error
+console.warn("warning message");
+console.info("info message");
+console.error("error message");
+//a=sumofnumbers(5+10);
+//console.log(a);
+
+//one line comment
+/*multiple line commeting line
+2 line 
+3rd line
+*/
+
+var a;
+console.log(a);
+var b=10;
+let c=20;
+const d= 200;
+
+console.log(b);
+console.log(c);
+console.log(d);
+
+b=1000;
+c=2000;
+//d=3000;
+
+console.log(b);
+console.log(c);
+console.log(d);
+
+var b=10;
+let c=20;
+const d= 200;
+console.log(b);
+console.log(c);
+console.log(d);
+
+{
+    var value1=100;
+    let value2=200;
+    const value3=300;
+    console.log(value1);
+    console.log(value2);
+    console.log(value3);
+}
+
+    console.log(value1);
+    console.log(value2);
+    console.log(value3);
